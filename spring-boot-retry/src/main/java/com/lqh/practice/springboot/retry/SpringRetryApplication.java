6dac77bd0e7c6e74a809f9bc9a0a08a67ff3e509
liqxhx <liqxhx@gmail.com>
2021-01-16 13:29:52 +0800

    add guava-retry

--- spring-boot-retry/src/main/java/com/lqh/practice/springboot/retry/SpringRetryApplication.java
+++ spring-boot-retry/src/main/java/com/lqh/practice/springboot/retry/SpringRetryApplication.java
@@ -5,7 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
 /**
- * <p> 类描述: SpringRetryApplication
+ * <pre> 类描述: SpringRetryApplication
+ * 参考：https://www.toutiao.com/i6881604908940788228/
+ *
+ * spring-retry 工具虽能优雅实现重试，但是存在两个不友好设计：
+ *
+ * 一个是重试实体限定为 Throwable 子类，说明重试针对的是可捕捉的功能异常为设计前提的，但是我们希望依赖某个数据对象实体作为重试实体， 但 sping-retry框架必须强制转换为Throwable子类。
+ *
+ * 另一个就是重试根源的断言对象使用的是 doWithRetry 的 Exception 异常实例，不符合正常内部断言的返回设计。
+ *
+ * Spring Retry 提倡以注解的方式对方法进行重试，重试逻辑是同步执行的，重试的“失败”针对的是Throwable， 如果你要以返回值的某个状态来判定是否需要重试，可能只能通过自己判断返回值然后显式抛出异常了。
+ *
+ * @Recover 注解在使用时无法指定方法，如果一个类中多个重试方法，就会很麻烦。
+ * </pre>
  *
  * @author qhlee
  * @version 1.0
---
+++ spring-boot-retry/src/test/java/com/lqh/practice/springboot/retry/guava/GuavaRetryTests.java
@@ -0,0 +1,18 @@
+package com.lqh.practice.springboot.retry.guava;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <p> 类描述: GuavaRetryTests
+ *
+ * @author qhlee
+ * @version 1.0
+ * @date 2021/01/16 12:10
+ * @since 2021/01/16 12:10
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class GuavaRetryTests {
+}
